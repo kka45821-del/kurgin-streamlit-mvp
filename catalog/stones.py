85ba868a1b75113191f3344a8ea04782843aabd9
kka45821-del <kka45821@gmail.com>
2026-05-24 18:32:33 +0300

    Create stones data module

---
+++ catalog/stones.py
@@ -0,0 +1,13 @@
+STONES = [
+    {
+        'shape': 'Круг',
+        'carat': 1.05,
+        'color': 'G',
+        'clarity': 'VVS1',
+        'score': 95,
+        'price': 32200,
+        'priceText': '32 200',
+        'diameter': 6.5,
+        'section': 'main'
+    }
+]
