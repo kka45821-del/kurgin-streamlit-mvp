4dffc854876a2ff9127b80a8bdefe330936d4a6d
kka45821-del <kka45821@gmail.com>
2026-05-25 15:33:32 +0300

    Add placeholder page module

---
+++ ui/pages/simple_pages.py
@@ -0,0 +1,8 @@
+def render_placeholder_page() -> str:
+    return """
+<div class="pageBody">
+  <div class="placeholder">
+    <div class="muted">Страница пока пустая. Сохраняем единый стиль, шапку и нижнюю навигацию. Наполнение подключим следующим шагом.</div>
+  </div>
+</div>
+"""
