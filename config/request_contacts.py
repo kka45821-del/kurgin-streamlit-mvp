a1290c35515658e98ab7d15968bdece6c363a92b
kka45821-del <kka45821@gmail.com>
2026-05-26 22:44:18 +0300

    Add request contacts config

---
+++ config/request_contacts.py
@@ -0,0 +1,6 @@
+REQUEST_CONTACTS = {
+    "phone": "+79688543140",
+    "max_url": "https://max.ru/u/f9LHodD0cOJeu7y7ABrIrsd118RD3qDsz7o5qmv7eDmW1Gi80MkpYFBCAhU",
+    "telegram_url": "https://t.me/cvdlab",
+    "whatsapp_url": "https://wa.me/79688543140",
+}
